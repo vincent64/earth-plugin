626c12b96fd2c67bb91b60ce10a977620ff174c2
Vincent Philippe <39349586+vincent64@users.noreply.github.com>
2024-02-12 18:31:56 +0100

    Create CommandEvent class

---
+++ src/main/java/com/vincent64/earthplugin/event/CommandEvent.java
@@ -0,0 +1,46 @@
+package com.vincent64.earthplugin.event;
+
+import com.vincent64.earthplugin.util.Messages;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerCommandSendEvent;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CommandEvent implements Listener {
+    private List<String> allowedUserCommands;
+
+    public CommandEvent() {
+        allowedUserCommands = Arrays.asList("help", "l", "region", "money", "pay", "rank", "home", "town", "map");
+    }
+
+    @EventHandler
+    public void onCommand(PlayerCommandSendEvent event) {
+        if(!event.getPlayer().isOp()) {
+            event.getCommands().retainAll(allowedUserCommands);
+        }
+    }
+
+    @EventHandler
+    public void onCommandExecution(PlayerCommandPreprocessEvent event) {
+        Player player = event.getPlayer();
+
+        //Check if the player is not an operator
+        if(!player.isOp()) {
+            //Get the command from the message
+            String command = event.getMessage().split(" ")[0];
+
+            //Check if the command is in the allowed commands list
+            for(String allowedCommand : allowedUserCommands) {
+                if(!command.equals("/" + allowedCommand)) {
+                    //Cancel the event
+                    event.setCancelled(true);
+                    player.sendMessage(Messages.playerCommandNotAllowed);
+                }
+            }
+        }
+    }
+}
